484b66c88483b65386eeca0729a8e6ad4b644bd0
sharmasmit <smeet.sharma007@gmail.com>
2024-02-06 14:46:25 +0530

    App.js updated

--- app.js
+++ app.js
@@ -27,15 +27,15 @@ class Drumkit {
         activeBars.forEach(bar => {
             bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
             if (bar.classList.contains("active")) {
-                if (bar.classList.contains(".kick-pad")) {
+                if (bar.classList.contains("kick-pad")) {
                     this.kickAudio.currentTime = 0;
                     this.kickAudio.play();
                 }
-                if (bar.classList.contains(".snare-pad")) {
+                if (bar.classList.contains("snare-pad")) {
                     this.snareAudio.currentTime = 0;
                     this.snareAudio.play();
                 }
-                if (bar.classList.contains(".hihat-pad")) {
+                if (bar.classList.contains("hihat-pad")) {
                     this.hihatAudio.currentTime = 0;
                     this.hihatAudio.play();
                 }
